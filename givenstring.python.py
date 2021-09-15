b989842bf9ac2ed070e5ac15d37749b38acb0fd5
ManikantaPadmanabhuni <79632996+MANIKANTAPADMANABHUNI@users.noreply.github.com>
2021-09-15 15:08:53 +0530

    Delete givenstring.python.py

--- givenstring.python.py
+++
@@ -1,7 +0,0 @@
-import re
-MyString1 = str(input( "enter the string:"))
-MyString2 =str(input("enter the search string:"))
-if re.search( MyString2, MyString1 ):
-    print("YES,string '{0}' is present in string '{1}' " .format(MyString2,MyString1))
-else:
-    print("NO,string '{0}' is not present in string {1} " .format(MyString2, MyString1) )
